b5907faf45765799f36c241880b68526997c02fa
Thea <lambert.tanner23@gmail.com>
2021-02-07 01:23:03 -0700

    home page with pic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,18 @@
 import React from "react";
-
+import image from "../computer.jpg";
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Minimalist Computer"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-black font-bold cursive leading-none lg:loading-snug home-name">
+          Greetings I'm Tanner
+        </h1>
+      </section>
+    </main>
+  );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,60 +4,60 @@ import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
   return (
-    <header className="bg-red-600">
+    <header className="bg-black">
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
           <NavLink
             to="/"
             exact
             activeClassName="text-white"
-            className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking widest"
+            className="inline-flex items-center py-6 px-3 mr-4 text-gray-400 hover:text-white text-4xl font-bold cursive tracking widest"
           >
-            Tanner
+            Tanner Lambert
           </NavLink>
           <NavLink
             to="/post"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-400 hover:text-white"
+            activeClassName="text-black bg-gray-600"
           >
             Blog Posts
           </NavLink>
           <NavLink
             to="/project"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-400 hover:text-white"
+            activeClassName="text-black bg-gray-600"
           >
             Projects
           </NavLink>
           <NavLink
             to="/about"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-400 hover:text-white"
+            activeClassName="text-black bg-gray-600"
           >
             About Me!
           </NavLink>
         </nav>
-        <div>
+        <div className="inline-flex py-3 px-3 my-6">
           <SocialIcon
             url="https://twitter.com/tllaxman23"
             className="mr-4"
             target="_blank"
             fgColor="#fff"
-            style={{ height: 35, width: 35 }}
+            style={{ height: 50, width: 50 }}
           />
           <SocialIcon
             url="https://www.youtube.com/channel/UCx9Aah334A7o9b8XTcBMavw"
             className="mr-4"
             target="_blank"
             fgColor="#fff"
-            style={{ height: 35, width: 35 }}
+            style={{ height: 50, width: 50 }}
           />
           <SocialIcon
-            url="www.linkedin.com/in/tanner-lambert"
+            url="https://www.linkedin.com/in/tanner-lambert"
             className="mr-4"
             target="_blank"
             fgColor="#fff"
-            style={{ height: 35, width: 35 }}
+            style={{ height: 50, width: 50 }}
           />
         </div>
       </div>
