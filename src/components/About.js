a1df7adc27eaec32825598fd53d7ed475b78bbe6
Thea <lambert.tanner23@gmail.com>
2021-02-18 00:04:27 -0700

    removing the about page temporatally

--- src/components/About.js
+++ src/components/About.js
@@ -2,23 +2,23 @@ import React, { useEffect, useState } from "react";
 import sanityClient from "../client";
 
 export default function About() {
-  const [author, setAuthor] = useState(null);
+  // const [author, setAuthor] = useState(null);
 
-  useEffect(() => {
-    sanityClient
-      .fetch(
-        `*[_type == "author"]{
-    name,
-    bio,
-    "authorImage": image.asset->url
+  // useEffect(() => {
+  //   sanityClient
+  //     .fetch(
+  //       `*[_type == "author"]{
+  //   name,
+  //   bio,
+  //   "authorImage": image.asset->url
 
-  }`
-      )
-      .then((data) => setAuthor(data[0]))
-      .catch(console.error);
-  } []);
+  // }`
+  //     )
+  //     .then((data) => setAuthor(data[0]))
+  //     .catch(console.error);
+  // } []);
 
-  if (!author) return <div>Loading...</div>;
+  // if (!author) return <div>Loading...</div>;
 
   return <h1>About page</h1>;
 }
