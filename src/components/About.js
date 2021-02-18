289a94c4d27ffb33cd3c0fa0ec7c514619cc8c55
Thea <lambert.tanner23@gmail.com>
2021-02-17 23:42:58 -0700

    post and single post

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client";
 
 export default function About() {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "author"]{
+    name,
+    bio,
+    "authorImage": image.asset->url
+
+  }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  } []);
+
+  if (!author) return <div>Loading...</div>;
+
   return <h1>About page</h1>;
 }
