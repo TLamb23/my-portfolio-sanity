136bd7c5e7a276abdb906793e4a5a45c84c2bfd7
Thea <lambert.tanner23@gmail.com>
2021-02-18 10:20:38 -0700

    adding about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,59 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../client";
+import utah from "../utah.jpg";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 export default function About() {
-  // const [author, setAuthor] = useState(null);
+  const [author, setAuthor] = useState(null);
 
-  // useEffect(() => {
-  //   sanityClient
-  //     .fetch(
-  //       `*[_type == "author"]{
-  //   name,
-  //   bio,
-  //   "authorImage": image.asset->url
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "author"]{
+    name,
+    bio,
+    "authorImage": image.asset->url
 
-  // }`
-  //     )
-  //     .then((data) => setAuthor(data[0]))
-  //     .catch(console.error);
-  // } []);
+  }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  }, []);
 
-  // if (!author) return <div>Loading...</div>;
+  if (!author) return <div>Loading...</div>;
 
-  return <h1>About page</h1>;
+  return (
+    <main className="relative">
+      <img src={utah} alt="Utah Landscape" className="absolute w-full" />
+      <div className="p-10 lg:pt-4 container mx-auto relative">
+        <section className="bg-gray-800 rounded-lg shadow-2xl lg:flex p-20">
+          <img
+            src={urlFor(author.authorImage).url()}
+            className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
+            alt={author.name}
+          />
+          <div className="text-lg flex flex-col justify-center">
+            <h1 className="cursive text-6xl text-gray-300 mb-4">
+              {" "}
+              Hey there. I'm{" "}
+              <span className="text-gray-100">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent
+                blocks={author.bio}
+                projectId="cv5ebtiw"
+                dataset="production"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 }
